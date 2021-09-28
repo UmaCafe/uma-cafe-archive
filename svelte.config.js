200875b88b6245571dc66fe49b1d1f606175073e
ProfessorSnep <github@mcsw.us>
2021-09-28 15:41:56 -0700

    update configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
+import node from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		target: '#root'
+		target: '#root',
+		adapter: node({})
 	}
 };
 
